996dce5a8199e992360b330b1e68ba1c7ddc5b78
Sanjoy Deb <sanjoydeb404@gmail.com>
2025-09-05 20:59:03 +0530

    mongoose

---
+++ day40/index.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/test")
+}
+
+main().then(()=>{
+    console.log("connection successful")
+}).catch((err)=>{
+    console.log(err)
+})
+
+// insertdata
+
+const userSchema = new  mongoose.Schema(
+    {
+        name : String,
+        email : String,
+        age : Number,
+    }
+
+)
+const User = mongoose.model("User" , userSchema );
+
+User.find({
+    age : {$gt : 20}
+}).then(res => (console.log(res[0].name))).catch((err)=> (console.log(err)))
+
+// User.insertMany([
+//     {
+//         name : "sanjoy ",
+//         email : "yahoo" ,
+//         age : 19
+//     },
+//     {
+//         name : "rahul ",
+//         email : "gmail" ,
+//         age : 25
+//     },
+//     {
+//         name : "trisha ",
+//         email : "good girl" ,
+//         age : 19
+//     }
+// ]).then((res)=> (console.log(res))).catch((err)=>{
+//     console.log(err)
+// })
+
+// const user1 = new User({
+//     name : "Sanjoy Deb" ,
+//     email : "sanjoydeb404@gmail.com",
+//     age : 19,
+// })
+
+
+
+// user1.save().then((res)=>{
+//     console.log(res);
+
+// }).catch((err)=>{
+//     console.log(err)
+// })
+
