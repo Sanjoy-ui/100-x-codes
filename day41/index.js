e6402b551e9791283b1d91c97205e497b13b8beb
Sanjoy Deb <sanjoydeb404@gmail.com>
2025-09-07 20:19:55 +0530

    restfuls

--- day41/index.js
+++ day41/index.js
@@ -4,11 +4,17 @@ import path from "path"
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import Chat from "./models/text.js";
+import methodOverride from 'method-override';
+
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(methodOverride("_method"));
 
 // app.use(express.static(path.join(__dirname) , "public"))
 
@@ -17,6 +23,22 @@ app.set("view engine" , "ejs");
 
 app.get("/" , (req,res)=>{
     res.send("working");
+})
+
+app.get("/chats/new", (req,res)=>{
+    res.render("form.ejs");
+});
+
+app.post("/chats",async (req ,res)=>{
+    let {from , message , to }= req.body ;
+    Chat.insertOne({
+        from : from,
+        message ,
+        to,
+        created_at : new Date()
+    }).then(res => console.log(res)).catch(err => console.log(err));
+    let data = await Chat.find()
+    res.render("index.ejs", { chats : data})
 })
 
 async function connectDb() {
@@ -34,9 +56,35 @@ async function connectDb() {
 
 // chat1.save().then(res => console.log(res)).catch(err => console.log(err))
 
+
+app.put("/chats/:id", async (req,res)=>{
+    let {id} = req.params;
+    let {message : newMsg} = req.body;
+    console.log(newMsg)
+    
+    try {
+        
+        let updateChat = await Chat.findByIdAndUpdate(id, {
+            message : newMsg,
+            created_at : new Date()
+        }, {runValidators : true , new : true})
+        console.log(updateChat)
+        res.redirect("/chats")
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Error updating chat");
+    }
+})
+
+app.get("/chats/:id/edit",async (req,res)=>{
+    let {id}= req.params;
+    let chat =await Chat.findById(id)
+    res.render("edit.ejs" , {chat})
+})
+
 app.get("/chats", async (req,res)=>{
     let chats = await Chat.find();
-   console.log(chats);
+//    console.log(chats);
     res.render("index.ejs" , { chats})
     console.log("working")
 })
---
+++ dsa/binarySearchInArray.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[]={5 ,11, 17,23,35,55,61,71,76,99};
+    int size_of_arr = sizeof(arr) / 4;
+    int start = 0 , end = size_of_arr ,mid ;
+    int target = 35;
+
+    
+    
+
+
+    return 0;
+}
