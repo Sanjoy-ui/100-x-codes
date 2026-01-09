db2056c00feb22dba3fe0cd3d04ee46332ab119c
Sanjoy Deb <sanjoydeb404@gmail.com>
2026-01-09 22:46:54 +0530

    sql prac

--- SQL_PRACTICE/index.js
+++ SQL_PRACTICE/index.js
@@ -1,22 +1,32 @@
 import { faker } from '@faker-js/faker';
+import mysql from 'mysql2/promise';
 
 const connection = await mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  database: 'test',
-  password: "mysql123"
+  database: 'practice_app',
+  password: "StrongPassword123"
 });
 
-const createRandomUser = ()=> {
-  return {
-    
-    userId: faker.string.uuid(),
-    username: faker.internet.username(),
-    email: faker.internet.email(),
-    avatar: faker.image.avatar(),
-    password: faker.internet.password(),
-    birthdate: faker.date.birthdate(),
-    registeredAt: faker.date.past(),
-  };
+try {
+    const [results] = await connection.query('SHOW TABLES');
+    console.log(results);
+} catch (error) {
+    console.log(error)
+} finally {
+    await connection.end();
 }
+
+// const createRandomUser = ()=> {
+//   return {
+
+//     userId: faker.string.uuid(),
+//     username: faker.internet.username(),
+//     email: faker.internet.email(),
+//     avatar: faker.image.avatar(),
+//     password: faker.internet.password(),
+//     birthdate: faker.date.birthdate(),
+//     registeredAt: faker.date.past(),
+//   };
+// }
 
